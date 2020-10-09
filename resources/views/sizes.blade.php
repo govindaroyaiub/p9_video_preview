aa9931c763167600d0ed171554337108a5cbdb30
Govinda Roy <govindaroy.ofc94@gmail.com>
2020-10-10 03:11:19 +0600

    Update

--- resources/views/sizes.blade.php
+++ resources/views/sizes.blade.php
@@ -11,18 +11,16 @@
     <div class="flex -mx-4">
         @include('sidebar')
         <div class="w-3/4 mx-4">
-            <div class="flex justify-between w-full">
-            <h3 class="text-xl font-semibold tracking-wide">Resolution Sizes</h3>
-                <a href="/sizes/add">
-                <button type="button"
-                    class="leading-tight bg-primary text-gray-200 rounded px-6 py-3 text-sm focus:outline-none focus:border-white">Add
-                    Size</Button>
-                </a>
-            </div>
-            <br>
             @include('alert')
             <br>
-            <h3 class="text-xl font-semibold tracking-wide">Resolution Sizes</h3>
+            <div class="flex justify-between w-full">
+                <h3 class="text-xl font-semibold tracking-wide">Resolution Sizes</h3>
+                <a href="/sizes/add">
+                    <button type="button"
+                        class="leading-tight bg-primary text-gray-200 rounded px-6 py-3 text-sm focus:outline-none focus:border-white">Add
+                        Size</Button>
+                </a>
+            </div>
             <br>
             <table id="datatable" class="stripe hover" style="width:100%; padding-top: 1em;  padding-bottom: 1em;">
                 <thead>
@@ -41,9 +39,10 @@
                         <td>{{ $size->name }}</td>
                         <td>{{ $size->width }}x{{ $size->height }}</td>
                         <td>
-                        <a href="/sizes/delete/{{$size->id}}">
-                        <button type="button" class="bg-red-500 text-gray-200 rounded hover:bg-red-400 px-4 py-2 focus:outline-none">Delete</button>
-                        </a>
+                            <a href="/sizes/delete/{{$size->id}}">
+                                <button type="button"
+                                    class="bg-red-500 text-gray-200 rounded hover:bg-red-400 px-4 py-2 focus:outline-none">Delete</button>
+                            </a>
                         </td>
                     </tr>
                     @endforeach
