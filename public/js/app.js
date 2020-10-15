949a351fe9f12bec50e41e61e4195447bd62a153
Govinda Roy <govindaroy.ofc94@gmail.com>
2020-10-15 22:40:26 +0600

    Update

--- public/js/app.js
+++ public/js/app.js
@@ -39215,8 +39215,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\p9_video_preview\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\p9_video_preview\resources\css\app.css */"./resources/css/app.css");
+__webpack_require__(/*! C:\xampp\htdocs\video_preview\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\video_preview\resources\css\app.css */"./resources/css/app.css");
 
 
 /***/ })
