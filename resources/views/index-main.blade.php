d241f7183225122032ae2407c58b4469e5a0d05f
Govinda Roy <govindaroy.ofc94@gmail.com>
2020-10-21 15:46:19 +0600

    Update

--- resources/views/index-main.blade.php
+++ resources/views/index-main.blade.php
@@ -232,7 +232,7 @@
                                                   d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"/>
                                         </svg>
                                     </a>
-
+                                    @if(mime_content_type(public_path('/poster_images/'.$project->poster_path)) == "image/gif")
                                     <a href="#"
                                        class="flex"
                                        onclick="document.querySelector('.companion-img').src='{{ asset('/poster_images/'.'/'.$project->poster_path) }}'; return false;">
@@ -244,6 +244,7 @@
                                                   d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"></path>
                                         </svg>
                                     </a>
+                                    @endif
                                 </div>
                             </div>
                         </div>
